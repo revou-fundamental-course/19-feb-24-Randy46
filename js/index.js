f6b2b3d053f78779585d24260d8c81f24a7fa912
Randy46 <randy21nov2003@gmail.com>
2024-02-26 02:01:14 +0800

    menambahkan section 1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+// slider
 let currentIndex = 0;
 const slider = document.getElementById('slider');
 const slides = document.querySelectorAll('.slider-item');
-let intervalId; // Variable to store interval ID
+let intervalId;
 
 document.addEventListener("DOMContentLoaded", function () {
-    currentIndex = 0; // Reset currentIndex
+    currentIndex = 0;
 
     function showSlide(index) {
         slides.forEach((slide, i) => {
@@ -17,19 +18,46 @@ document.addEventListener("DOMContentLoaded", function () {
         showSlide(currentIndex);
     }
 
-    intervalId = setInterval(nextSlide, 3000); // Assign interval ID to variable
+    intervalId = setInterval(nextSlide, 3000);
 
-    // Pause interval on slider hover
+
     slider.addEventListener('mouseenter', function () {
         clearInterval(intervalId);
     });
 
-    // Resume interval on slider mouse leave
+
     slider.addEventListener('mouseleave', function () {
         intervalId = setInterval(nextSlide, 3000);
     });
 
     showSlide(currentIndex);
+});
+
+//vision
+document.addEventListener("DOMContentLoaded", function () {
+    const vision = document.querySelector('.vision');
+
+    function isInViewport(element) {
+        if (!element) return false;
+
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function handleScroll() {
+        if (isInViewport(vision)) {
+            vision.classList.add('in-view');
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
 });
 
 
@@ -55,6 +83,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     window.addEventListener('scroll', handleScroll);
-    handleScroll(); // Check on page load
+    handleScroll();
 });
 
